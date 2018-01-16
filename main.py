16194ba6424a2fe095b358905437836a59c0aec1
Sebastian Appelt <sebastian.appelt@web.de>
2018-01-16 01:41:03 +0100

    Added configuration via json

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from anki_object_detection.anki_camera import AnkiCamera
-import argparse
 from anki_object_detection.line import Line
+import argparse
+import json
+
 
 
 def main():
 
     parser = argparse.ArgumentParser(description='Detect a blue cube')
-    #parser.add_argument('verticalLeftLane', type=int, nargs=4,
-    #                    help='the vertical left lane x1 y1 x2 y2')
+    parser.add_argument('configFile', nargs='?',  default='config.json')
 
     #parser.add_argument('verticalRightLane', type=int, nargs=4,
     #                    help='the vertical right lane x1 y1 x2 y2')
@@ -20,14 +21,20 @@ def main():
 
     args = parser.parse_args()
 
+    with open(args.configFile) as json_data:
+        config = json.load(json_data)
+    print(config)
+
     #kafka = Kafka()
     #loop = asyncio.get_event_loop()
     #messageGateway = Websocket(loop)
 
-    max_left_lane = Line(542, 332, 472, 898)
-    max_right_lane = Line(789, 906, 470, 880)
-    max_horizontal_upper_lane = Line(260, 1062, 579, 535)
-    max_horizontal_lower_lane = Line(132, 1163, 718, 653)
+    max_left_lane = Line(config['maxLeftLane']['x1'], config['maxLeftLane']['x2'], config['maxLeftLane']['y1'], config['maxLeftLane']['y2'])
+    max_right_lane = Line(config['maxRightLane']['x1'], config['maxRightLane']['x2'], config['maxRightLane']['y1'], config['maxRightLane']['y2'])
+    max_horizontal_upper_lane = Line(config['maxHorizontalUpperLane']['x1'], config['maxHorizontalUpperLane']['x2'], config['maxHorizontalUpperLane']['y1'],
+                                     config['maxHorizontalUpperLane']['y2'])
+    max_horizontal_lower_lane = Line(config['maxHorizontalLowerLane']['x1'], config['maxHorizontalLowerLane']['x2'], config['maxHorizontalLowerLane']['y1'],
+                                     config['maxHorizontalLowerLane']['y2'])
 
     ankiCamera = AnkiCamera()
     try:
